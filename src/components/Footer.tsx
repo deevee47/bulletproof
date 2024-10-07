4f0571dbd7236f51ea4c96f93caf18c176037ac6
Suryanshu Mittal <100358189+Alexander-Lucifer@users.noreply.github.com>
2024-10-07 18:55:59 +0530

    navbar and footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+const Footer= () => {
+  return (
+    <footer className="footer">
+      <p>Namaste 🙏</p>
+      <p>From ATC Tech Team</p>
+    </footer>
+  );
+};
+
+export default Footer;
