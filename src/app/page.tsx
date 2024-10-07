7ac04b4f5cf78cb41bbf31457b10fe5987124e30
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-10-07 20:18:20 +0530

    removed unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import GuessSpeaker from "@/components/GuessSpeaker";
 import Landing from "@/components/Landing";
 import TopographicBackground from "@/components/TopographicBackground";
-// import { Marquee3D } from "@/components/Marquee3D";
 import PreviousBulletProof from "@/components/PreviousBulletProof";
 
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 
 
 export default function Home() {
