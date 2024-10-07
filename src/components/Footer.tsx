1c1b77baa59f79d63a9802c48b553ecf98052418
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-10-07 20:31:41 +0530

    updated text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ const Footer= () => {
       <nav className="relative px-4 py-6 backdrop-blur-md bg-black/20 border border-white/10 shadow-lg text-gray-400">
         <ul className="flex justify-center items-center gap-2">
           {/* <p>Namaste 🙏</p> */}
-          <p>🙏</p>
-          <p>From ATC Team, Fun Fact: The background is all mathematics, no image :P</p>
+          <p className='DAMN this CODE'>🙏</p>
+          <p>From ATC Team, Fun Fact: This website background is all mathematics, no image :P</p>
         </ul>
       </nav>
      
