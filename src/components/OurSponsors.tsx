ed50287b17c96b9840f75d5f4359509d40ee0482
Adarsh <adarsh71586@gmail.com>
2024-10-11 16:38:08 +0530

    one more sponsor damn

--- src/components/OurSponsors.tsx
+++ src/components/OurSponsors.tsx
@@ -10,7 +10,7 @@ const OurSponsors = () => {
         Our Sponsors
       </h1>
       <div className='flex justify-center items-center'>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 w-full max-w-3xl">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 w-full max-w-3xl">
           {sponsorImages.map((image, index) => (
             <div key={index} className="flex justify-center items-center">
               <div className="relative w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32">
