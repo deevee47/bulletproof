923a10e8d4796069653c0a564f6572c2db8358a7
Adarsh <adarsh71586@gmail.com>
2024-10-11 16:17:30 +0530

    added 5th sponsor

--- src/components/OurSponsors.tsx
+++ src/components/OurSponsors.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 
 const OurSponsors = () => {
-  const sponsorImages = ['/1.png', '/s2.jpg', '/3.png', '/4.png'];
+  const sponsorImages = ['/1.png', '/s2.jpg', '/3.png', '/4.png', '/s5.png'];
 
   return (
     <div className="container mx-auto px-4 py-6 my-4">
@@ -10,7 +10,7 @@ const OurSponsors = () => {
         Our Sponsors
       </h1>
       <div className='flex justify-center items-center'>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 w-full max-w-3xl">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 w-full max-w-3xl">
           {sponsorImages.map((image, index) => (
             <div key={index} className="flex justify-center items-center">
               <div className="relative w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32">
