1f1a72aa983a907a4cf57f14c12d6f8598e6cdc1
Suryanshu Mittal <100358189+Alexander-Lucifer@users.noreply.github.com>
2024-10-12 21:59:26 +0530

    Styling Titles

--- src/components/EventCards.tsx
+++ src/components/EventCards.tsx
@@ -21,6 +21,11 @@ const speakers = [
 export default function EveCards() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-wrap justify-center items-center p-4 gap-4">
+      <div className="container mx-auto px-4 py-6 my-4">
+      <h1 className="text-center pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-3xl md:text-4xl lg:text-5xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10 mb-8 relative z-10">
+        Events
+      </h1>
+      </div>
       {speakers.map((speaker, index) => (
         <EventCard key={index} {...speaker} />
       ))}
