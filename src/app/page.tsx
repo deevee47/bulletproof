b681770f10f3fcaa30357bac677cc5c7a56b7423
Suryanshu Mittal <100358189+Alexander-Lucifer@users.noreply.github.com>
2024-10-18 23:34:41 +0530

    del

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import GuessSpeaker from "@/components/GuessSpeaker";
 import Landing from "@/components/Landing";
 import TopographicBackground from "@/components/TopographicBackground";
-import OurSponsors from "@/components/OurSponsors";
-import Card from "@/components/Card";
-
+import BentoDemo from "@/components/Bento";
 export default function Home() {
   return (
     <div className="bg-black">
       <TopographicBackground />
       <Landing />
-      <Card />
-      <OurSponsors />
-      
-      <GuessSpeaker />
+      <BentoDemo />
       
     </div>
   );
